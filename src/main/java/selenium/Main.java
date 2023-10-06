8d756c93f40424e544e485d919b9d0835bdc178c
Kamil Witkowski <kamil.witt05@gmail.com>
2023-10-06 08:46:16 +0200

    first commit

---
+++ src/main/java/selenium/Main.java
@@ -0,0 +1,12 @@
+package selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://facebook.com");
+        }
+    }
